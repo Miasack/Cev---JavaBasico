f28041faadff49a7ce482403016572309574fcbf
Roberto Miasack <40808057+Miasack@users.noreply.github.com>
2018-07-18 22:06:02 -0300

    Initial commit

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author Root
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc = 0; cc <= 3; cc++){
+            System.out.println(cc);
+        }
+        /* Estrutura com while
+            int cc = 0;
+            while (cc = 4) {
+                System.out.println(cc);
+                cc++;
+        }
+        */
+       
+    }
+    
+}
---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author Root
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        String nome = "Roberto";
+        float nota = 8.5f;
+        System.out.printf("Anota de %s é %.1f \n", nome, nota);
+        
+    }
+    
+}
